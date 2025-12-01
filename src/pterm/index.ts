7a4cf7ce7402c9450877108c8c40f6fd72784b09
rboudrouss <r.boudrouss@gmail.com>
2025-12-02 00:30:48 +0100

    update index

--- src/pterm/index.ts
+++ src/pterm/index.ts
@@ -4,9 +4,10 @@ import type { PTerm, pTermImplementation, evalContext } from "../general-types.t
 import { varPTermImplementation } from "./var.ts";
 import { absPTermImplementation } from "./abs.ts";
 import { appPTermImplementation } from "./app.ts";
+import { PTermRegistry } from "../general-types.ts";
 
 // Collect all implementations
-const registry = {
+const registry: { [K in PTerm["type"]]: pTermImplementation<PTermRegistry[K]> } = {
   ...varPTermImplementation,
   ...absPTermImplementation,
   ...appPTermImplementation,
@@ -34,8 +35,8 @@ export function resetVarCounter(): void {
 // Combined operations (tying the recursive knot)
 // =============================================================================
 
-function getImpl(t: PTerm) {
-  const impl = registry[t.type];
+function getImpl<K extends PTerm["type"]>(t: PTerm & { type: K }): pTermImplementation<PTermRegistry[K]> {
+  const impl = registry[t.type as K];
 
   if (!impl) {
     // exhaustiveness check, if next line can be reached, typescript would've complained
