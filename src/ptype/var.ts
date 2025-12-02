1971bc9e442a489cc615604cec5c4566afab5762
rboudrouss <r.boudrouss@gmail.com>
2025-12-02 01:43:47 +0100

    var type

---
+++ src/ptype/var.ts
@@ -0,0 +1,51 @@
+import type { pTypeImplementation, PType } from "../types.ts";
+
+const tVarPTypeName = "TVar" as const;
+
+function tVarConstructor(arg: { name: string }) {
+  return { type: tVarPTypeName, name: arg.name };
+}
+
+type tVarType = ReturnType<typeof tVarConstructor>;
+
+declare module "../types.ts" {
+  interface PTypeRegistry {
+    [tVarPTypeName]: tVarType;
+  }
+}
+
+const tVarFreeVarsCollector = (
+  _recurse: (ty: PType) => Set<string>,
+  ty: tVarType
+): Set<string> => new Set([ty.name]);
+
+const tVarTypeSubstitution = (
+  _recurse: (ty: PType, v: string, t0: PType) => PType,
+  v: string,
+  t0: PType,
+  ty: tVarType
+): PType => (ty.name === v ? t0 : ty);
+
+const tVarContainsVar = (
+  _recurse: (varName: string, ty: PType) => boolean,
+  varName: string,
+  ty: tVarType
+): boolean => ty.name === varName;
+
+const tVarPrint = (
+  _recurse: (ty: PType) => string,
+  ty: tVarType
+): string => ty.name;
+
+export const tVarPTypeImplementation = {
+  [tVarPTypeName]: {
+    pTypeName: tVarPTypeName,
+    constructor: tVarConstructor,
+    freeVarsCollector: tVarFreeVarsCollector,
+    typeSubstitution: tVarTypeSubstitution,
+    containsVar: tVarContainsVar,
+    print: tVarPrint,
+  } as pTypeImplementation<tVarType>,
+};
+
+export { tVarConstructor };
